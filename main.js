fc1017ceb750f8ec5cee887da8a97567e23d851a
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-02-22 17:28:06 -0300

    add footer and 'back to top' button

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for (const link of links) {
 }
 
 /* Mudar o header da página quando der o scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper', {
@@ -52,5 +52,23 @@ scrollReveal.reveal(
     #about .image, #about .text, 
     #services header, #services .card, 
     #testimonials header, #testimonials .testimonials, 
-    #contact .text, #contact .links`, 
+    #contact .text, #contact .links,
+    footer .brand, footer .social`, 
     { interval: 100 })
+
+/* Botão de voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* Quando usar o scroll */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
